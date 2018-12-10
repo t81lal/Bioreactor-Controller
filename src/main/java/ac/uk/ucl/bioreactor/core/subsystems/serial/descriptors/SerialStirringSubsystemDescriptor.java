3ba5603e50bd430545c912d769b962c8247af7e4
t81lal <biblthebest@hotmail.com>
2018-12-10 14:31:04 +0100

    Fix type in StirringSubsystemDescriptor

--- src/main/java/ac/uk/ucl/bioreactor/core/subsystems/serial/descriptors/SerialStirringSubsystemDescriptor.java
+++ src/main/java/ac/uk/ucl/bioreactor/core/subsystems/serial/descriptors/SerialStirringSubsystemDescriptor.java
@@ -2,10 +2,11 @@ package ac.uk.ucl.bioreactor.core.subsystems.serial.descriptors;
 
 import ac.uk.ucl.bioreactor.core.Context;
 import ac.uk.ucl.bioreactor.core.subsystems.Subsystem;
+import ac.uk.ucl.bioreactor.core.subsystems.descriptors.StirringSubsystemDescriptor;
 import ac.uk.ucl.bioreactor.core.subsystems.descriptors.TemperatureSubsystemDescriptor;
 import ac.uk.ucl.bioreactor.core.subsystems.serial.SerialTemperatureSubsystem;
 
-public class SerialStirringSubsystemDescriptor extends TemperatureSubsystemDescriptor {
+public class SerialStirringSubsystemDescriptor extends StirringSubsystemDescriptor {
 
 	@Override
 	public Subsystem createSubsystem(Context context) {
